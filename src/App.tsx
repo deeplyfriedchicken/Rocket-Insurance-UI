b04f9d6dd5197ca0448e3638d43c7719df9df483
kcunanan <kevin.a.cunanan@gmail.com>
2020-10-04 15:59:46 -0700

    added theme, store, and typography update to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './assets/main.css';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createMuiTheme, Theme, ThemeOptions, ThemeProvider } from '@material-ui/core/styles';
+
+import Store from './store/Store';
+import Layout from './components/Layout/Layout';
 
 console.log(process.env.REACT_APP_API_URL);
 
+const themeOptions: ThemeOptions = {
+  palette: {
+    background: {
+      default: 'fff',
+    },
+    primary: {
+      light: '#243862',
+      main: '#212446',
+      dark: '#212446',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#5b5f6f',
+      main: '#47AB8C',
+      dark: '#207F8C ',
+      contrastText: '#fff',
+    },
+  },
+  typography: {
+    fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
+  },
+}
+
+const theme: Theme = createMuiTheme(themeOptions);
+
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <Store>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Layout />
+      </Router>
+    </ThemeProvider>
+  </Store>
 );
 
 export default App;
