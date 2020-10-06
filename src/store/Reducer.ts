7b2b67008e74b4af3f4f9868f32cc23506376cb3
kcunanan <kevin.a.cunanan@gmail.com>
2020-10-06 16:38:19 -0700

    reducer and types

--- src/store/Reducer.ts
+++ src/store/Reducer.ts
@@ -1,27 +1,32 @@
-import { State, ActionTypes } from './types';
+import { initialState } from './Store';
+import { RESET, UPDATE_RATINGS, UPDATE_QUOTE, UPDATE_PAGE_LOADING, UPDATE_PREMIUM_LOADING, State, ActionTypes } from './types';
 
 const Reducer = (state: State, action: ActionTypes): State => {
   switch (action.type) {
-    case 'UPDATE_RATINGS':
+    case UPDATE_RATINGS:
       return {
         ...state,
         ratings: action.payload,
       };
-    case 'UPDATE_QUOTE':
+    case UPDATE_QUOTE:
       return {
         ...state,
         quote: action.payload,
       };
-    case 'UPDATE_PAGE_LOADING':
+    case UPDATE_PAGE_LOADING:
       return {
         ...state,
         pageLoading: action.payload,
       };
-    case 'UPDATE_PREMIUM_LOADING':
+    case UPDATE_PREMIUM_LOADING:
       return {
         ...state,
         premiumLoading: action.payload,
       };
+    case RESET:
+      return {
+        ...initialState,
+      }
     default:
       return state;
   }
