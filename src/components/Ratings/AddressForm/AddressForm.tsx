650b65be19af6d3ba61a5a4aeffdc6e087325217
kcunanan <kevin.a.cunanan@gmail.com>
2020-10-05 19:58:10 -0700

    split forms into separate components

---
+++ src/components/Ratings/AddressForm/AddressForm.tsx
@@ -0,0 +1,117 @@
+import React from 'react'
+
+import { UPDATE_RATINGS } from '../../../store/types';
+
+import { Button, Grid } from '@material-ui/core';
+import { useForm } from 'react-hook-form';
+
+import { Context } from '../../../store/Store';
+
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+import ControlledTextInput from '../../Common/ControlledTextInput';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    buttonContainer: {
+      '& button': {
+        margin: '12px',
+      },
+    },
+    rightButtonContainer: {
+      textAlign: 'right',
+    },
+    formContainer: {
+      marginBottom: '16px',
+    },
+  }),
+);
+
+interface Props {
+  onSubmit(): void;
+  handleBack(): void;
+}
+
+const AddressForm: React.FC<Props> = ({ handleBack, onSubmit }) => {
+  const { state: { ratings }, dispatch } = React.useContext(Context);
+  const { address } = ratings;
+  const { handleSubmit, errors, control } = useForm({ defaultValues: { ...address }, shouldUnregister: false });
+  const classes = useStyles();
+
+  return (
+    <form
+      onSubmit={handleSubmit(() => {
+        onSubmit();
+      })}
+    >
+      <Grid className={classes.formContainer} container spacing={3}>
+        <Grid item sm={7}>
+          <ControlledTextInput
+            control={control}
+            defaultValue={address.line1}
+            error={errors?.line1}
+            handleChange={(e): void => dispatch({ type: UPDATE_RATINGS, payload: { ...ratings, address: { ...address, line1: e.target.value }} })}
+            label="Address"
+            name="line1"
+            rules={{ required: "This field is required" }}
+          />
+        </Grid>
+        <Grid item sm={5}>
+          <ControlledTextInput
+            control={control}
+            defaultValue={address.line2}
+            error={errors?.line2}
+            handleChange={(e): void => dispatch({ type: UPDATE_RATINGS, payload: { ...ratings, address: { ...address, line2: e.target.value }} })}
+            label="Apt #"
+            name="line2"
+            rules={{ required: "This field is required" }}
+          />
+        </Grid>
+        <Grid item sm={4}>
+          <ControlledTextInput
+            control={control}
+            defaultValue={address.city}
+            error={errors?.city}
+            handleChange={(e): void => dispatch({ type: UPDATE_RATINGS, payload: { ...ratings, address: { ...address, city: e.target.value }} })}
+            label="City"
+            name="city"
+            rules={{ required: "This field is required" }}
+          />
+        </Grid>
+        <Grid item sm={4}>
+          <ControlledTextInput
+            control={control}
+            defaultValue={address.region}
+            error={errors?.region}
+            handleChange={(e): void => dispatch({ type: UPDATE_RATINGS, payload: { ...ratings, address: { ...address, region: e.target.value }} })}
+            label="Region"
+            name="region"
+            rules={{ required: "This field is required" }}
+          />
+        </Grid>
+        <Grid item sm={4}>
+          <ControlledTextInput
+            control={control}
+            defaultValue={address.postal}
+            error={errors?.postal}
+            handleChange={(e): void => dispatch({ type: UPDATE_RATINGS, payload: { ...ratings, address: { ...address, postal: e.target.value }} })}
+            label="Postal"
+            name="postal"
+            rules={{ required: "This field is required" }}
+          />
+        </Grid>
+        <Grid container spacing={3}>
+          <Grid className={classes.buttonContainer} item sm={2}>
+            <Button color="secondary" variant="contained" type="button" onClick={handleBack}>BACK</Button>
+          </Grid>
+          <Grid className={`${classes.rightButtonContainer} ${classes.buttonContainer}`} item sm={10}>
+            <Button variant="contained" type="button">Reset</Button>
+            <Button color="primary" variant="contained" type="submit">Submit</Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </form>
+  );
+}
+
+export default AddressForm;
---
+++ src/components/Ratings/NameForm/NameForm.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { UPDATE_RATINGS } from '../../../store/types';
+
+import { useForm } from 'react-hook-form';
+import { Button, Grid } from '@material-ui/core';
+
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+import { Context } from '../../../store/Store';
+import ControlledTextInput from '../../Common/ControlledTextInput';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    subtitle: {
+      color: theme.palette.primary.contrastText,
+      fontWeight: 500,
+      fontSize: '32px',
+      marginBottom: '16px',
+    },
+    paragraph: {
+      color: theme.palette.primary.contrastText,
+      fontSize: '18px',
+      marginBottom: '16px',
+    },
+    formContainer: {
+      marginBottom: '16px',
+    },
+    textField: {
+      '& .MuiFilledInput-input': {
+        color: '#000',
+      },
+      '& .MuiFilledInput-root': {
+        backgroundColor: 'white',
+        '&:focused': {
+          backgroundColor: 'white',
+        }
+      },
+      '& .MuiFormHelperText-root': {
+        fontWeight: 600,
+      },
+    },
+  }),
+);
+
+interface Props {
+  onSubmit(): void;
+}
+
+const NameForm: React.FC<Props> = ({ onSubmit }) => {
+  const classes = useStyles();
+  const { state: { ratings }, dispatch } = React.useContext(Context);
+  const { firstName, lastName } = ratings;
+  const { handleSubmit, errors, control } = useForm({ defaultValues: { firstName, lastName }, shouldUnregister: false });
+
+  return (
+    <form
+      onSubmit={handleSubmit(() => {
+        onSubmit();
+      })}
+    >
+      <Grid className={classes.formContainer} container spacing={3}>
+        <Grid item sm={6}>
+          <ControlledTextInput
+            control={control}
+            defaultValue={firstName}
+            error={errors?.firstName}
+            handleChange={(e): void => dispatch({ type: UPDATE_RATINGS, payload: { ...ratings, firstName: e.target.value }})}
+            label="First Name"
+            name="firstName"
+            rules={{ required: "This field is required" }}
+          />
+        </Grid>
+        <Grid item sm={6}>
+          <ControlledTextInput
+            control={control}
+            defaultValue={lastName}
+            error={errors?.lastName}
+            handleChange={(e): void => dispatch({ type: UPDATE_RATINGS, payload: { ...ratings, lastName: e.target.value }})}
+            label="Last Name"
+            name="lastName"
+            rules={{ required: "This field is required" }}
+          />
+        </Grid>
+      </Grid>
+      <Button
+        color="secondary"
+        variant="contained"
+        type="submit"
+      >
+          NEXT
+        </Button>
+    </form>
+  )
+};
+
+export default NameForm;
