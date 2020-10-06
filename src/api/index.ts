99c9eba936e5431914dc768777509ec77d115494
kcunanan <kevin.a.cunanan@gmail.com>
2020-10-06 13:19:56 -0700

    updated api to use fetch over axios to get thru cors policy

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,31 +1,30 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import axios from 'axios';
-
 import { Quote, Ratings, UpdatedQuote } from '../store/types';
 
 const { REACT_APP_API_URL: baseURL } = process.env;
 
-const instance = axios.create({
-  baseURL,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
-  timeout: 1000,
-})
-
 export const createQuote = async (ratings: Ratings): Promise<Quote | undefined> => {
   try {
-    const { data } = await instance.post('/quotes', {
-      "first_name": ratings.firstName,
-      "last_name": ratings.lastName,
-      "address": {
-        "line_1": ratings.address.line1,
-        "line2": ratings.address.line2,
-        "city": ratings.address.city,
-        "region": ratings.address.region,
-        "postal": ratings.address.postal,
-      }
-    });
+    const URL = `${baseURL}/quotes`;
+    const init = {
+      method: 'POST',
+      mode: 'cors' as RequestMode,
+      body: JSON.stringify({
+        "first_name": ratings.firstName,
+        "last_name": ratings.lastName,
+        "address": {
+          "line_1": ratings.address.line1,
+          "line2": ratings.address.line2,
+          "city": ratings.address.city,
+          "region": ratings.address.region,
+          "postal": ratings.address.postal,
+        },
+      }),
+    };
+
+    const request = new Request(URL, init);
+    const response = await fetch(request);
+    const data: Record<string, Quote> = await response.json();
     return data?.quote;
   }
   catch (err) {
@@ -35,8 +34,16 @@ export const createQuote = async (ratings: Ratings): Promise<Quote | undefined> 
 }
 
 export const updateQuote = async (quoteData: UpdatedQuote): Promise<Quote | undefined> => {
+  const URL = `${baseURL}/quotes/${quoteData.quoteId}`;
+  const init = {
+    method: 'PUT',
+    mode: 'cors' as RequestMode,
+    body: JSON.stringify({ quote: quoteData }),
+  };
   try {
-    const { data } = await instance.put(`/quotes/${quoteData.quoteId}`, { quote: quoteData });
+    const request = new Request(URL, init);
+    const response = await fetch(request);
+    const data: Record<string, Quote> = await response.json();
     return data?.quote;
   }
   catch(err) {
