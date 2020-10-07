e81cd14caca962aab7c1252ad0a05fe793fd8f08
kcunanan <kevin.a.cunanan@gmail.com>
2020-10-06 17:24:03 -0700

    added more marging and fontsize change on xs

--- src/components/Layout/ContainerLayout.tsx
+++ src/components/Layout/ContainerLayout.tsx
@@ -14,25 +14,38 @@ interface Props {
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    root: {
+      margin: '1rem 0',
+      padding: '0 1rem',
+      [theme.breakpoints.down('xs')]: {
+        margin: '3rem 0',
+      }
+    },
     subtitle: {
       color: theme.palette.primary.contrastText,
       fontWeight: 500,
       fontSize: '32px',
       marginBottom: '16px',
+      [theme.breakpoints.down('xs')]: {
+        fontSize: '1.5rem',
+      }
     },
     paragraph: {
       color: theme.palette.primary.contrastText,
       fontSize: '18px',
       marginBottom: '16px',
+      [theme.breakpoints.down('xs')]: {
+        fontSize: '1rem',
+      }
     },
   }),
 );
 
-const ContainerLayout: React.FC<Props> = ({ className, heading, subtitle, paragraph, children }) => {
+const ContainerLayout: React.FC<Props> = ({ className = '', heading, subtitle, paragraph, children }) => {
   const classes = useStyles();
 
   return (
-    <div className={className}>
+    <div className={`${className} ${classes.root}`}>
       <Heading text={heading} />
       <Typography className={classes.subtitle} variant="subtitle1">
         {subtitle}
